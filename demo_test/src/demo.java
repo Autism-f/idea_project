85c9412a94c5581942c67d3baa8b94be1a51625c
ethan <1786633672@qq.com>
2020-05-06 10:31:05 +0800

    fourth submit

--- demo_test/src/demo.java
+++ demo_test/src/demo.java
@@ -4,5 +4,8 @@ public class demo {
         System.out.println("Hello World!");
         System.out.println("111");
         System.out.println("我下班了");
+        for (int i = 0; i < 10; i++){
+            System.out.println(i);
+        }
     }
 }
