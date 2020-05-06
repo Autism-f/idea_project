8315ab23dc63359e997f90124e557dc57207652e
ethan <1786633672@qq.com>
2020-05-06 10:44:24 +0800

    welcome submit test

--- demo_test/src/demo.java
+++ demo_test/src/demo.java
@@ -3,7 +3,6 @@ public class demo {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("111");
-        System.out.println("我下班了");
         for (int i = 0; i < 10; i++){
             System.out.println(i);
         }
